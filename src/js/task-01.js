923870bca2d3faeb6837a5dd1a7fa4707a9cdb6f
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-07-07 23:28:47 +0300

    Task-1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -4,4 +4,55 @@
 // 4. Вывести в консоль количество li в ul;
 
 
-const 
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+
+const numbersOfCategories = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${numbersOfCategories.length}`);
+
+
+const listOfCategoriesItems = document.querySelectorAll('.item');
+
+
+const firstTitleCategories = numbersOfCategories[0].querySelector('h2').textContent;
+
+const firstElementOfList = listOfCategoriesItems[0].childNodes[3].children.length;
+
+console.log(`Elements: ${firstElementOfList}`);
+
+console.log(`Category: ${firstTitleCategories}`);
+
+
+
+const secondTitleCategories = numbersOfCategories[1].querySelector('h2').textContent;
+
+
+const seondElementOfList = listOfCategoriesItems[1].childNodes[3].children.length;
+
+console.log(`Elements: ${seondElementOfList}`);
+
+console.log(`Category: ${secondTitleCategories}`);
+
+
+const thirdTitleCategories = numbersOfCategories[2].querySelector('h2').textContent;
+
+
+const thirdElementOfList = listOfCategoriesItems[2].childNodes[3].children.length;
+
+console.log(`Elements: ${thirdElementOfList}`);
+
+console.log(`Category: ${thirdTitleCategories}`);
+
+
+
+
