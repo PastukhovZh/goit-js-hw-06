1e692b1382763bf69f1bfdc45154fe66f159dbc5
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-07-16 12:35:02 +0300

    task~6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,21 +1,21 @@
 // Напиши скрипт, который при потере фокуса на инпуте (событие blur), проверяет его содержимое на правильное количество введённых символов.
 
-const inputValue = document.querySelector('[data-length]')
-const inputValue1 = document.querySelector('#validation-input')
-const validationInputValid = document.querySelector('style')
+const inputValue = document.querySelector('[data-length]');
+const getAtributeLength = inputValue.getAttribute('data-length');
+
+
+const validationInputValid = document.querySelector('');
+const typeofString = typeof '';
+
+
+
 
 
 // inputValue.addEventListener('blur', onInputValueBlur);
 
 // function onInputValueBlur() {
-        
-//         if (typeof "" === "") {
-//                 return
-//         }
-        
-        
-//         console.log('Bye');
+// typeofString.length === getAtributeLength ?
 // }
 
-console.log(inputValue)
-console.log(inputValue1)
+console.log(validationInputValid)
+console.log(getAtributeLength)
